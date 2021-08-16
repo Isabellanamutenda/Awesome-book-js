cb0818fabacc65081736bf5486d45d1e7386ecda
olipliche <olipliche@gmail.com>
2021-08-16 23:37:37 +0100

    Add JS file

--- index.js
+++ index.js
@@ -1,4 +1,61 @@
-const listOfBooks = [ {title: "Lorem ipsum", Author: "Author0", Id: 0},
-{title: "Testeroo", Author: "Author1", Id: 1},
-{title: "Second book", Author: "Author2", Id: 2},
-]
+// Book Class: Represents a Book
+ class Book { 
+   constructor(title, author, isbn) {
+     this.title = title;
+     this.author = author;
+     this.isbn = isbn;
+   }
+ }
+// UI Class: Handle UI Tasks
+  class UI {
+    static displayBooks () {
+      const StoredBooks = [
+        {
+          title: 'Lorem ipsum',
+          author: Testeroo Testyy,
+          isbn: '5474746',
+        },
+        {
+          title: 'Second book',
+          author: Testeroo Testyy,
+          isbn: '8765335',
+        }
+      ];
+
+      const books = StoredBooks;
+
+      books.forEach((book) => UI.addBookToList(book));
+    }
+     static addBookToList(book) {
+       const list = document.querySelector('#book-list');
+
+       const row = document.createElement('tr');
+
+       row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#">x</a></td>
+       `;
+
+       list.appendChild(row);
+     }
+  }
+//  Store Class: Handles Storage
+
+// Event: Display Books
+ document.addEventListener('DOMContentLoaded', UI.displayBooks);
+// Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+  //  instatiate book 
+  const book = new Book(title, author, isbn);
+   console.log(book)
+});
+
+// Event: Remove a Book
