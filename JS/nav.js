5ed5aed508839574ed022b2e4b13e6e775de8c62
olipliche <olipliche@gmail.com>
2021-08-20 05:19:02 +0100

    Update files

--- JS/nav.js
+++ JS/nav.js
@@ -1,16 +1,83 @@
 /* eslint no-undef: 0 */
-show = (section) => {
-  const toshow = document.getElementById(section.id);
-  toshow.classList.replace('invisible', 'visible');
-  for (let i = 1; i <= 3; i += 1) {
-    if (`s${String(i)}` !== section.id) {
-      document.getElementById(`s${String(i)}`).classList.replace('visible', 'invisible');
-    }
-  }
-};
+// show = (section) => {
+//   const toshow = document.getElementById(section.id);
+//   toshow.classList.replace('invisible', 'visible');
+//   for (let i = 1; i <= 3; i += 1) {
+//     if (`s${String(i)}` !== section.id) {
+//       document.getElementById(`s${String(i)}`).classList.replace('visible', 'invisible');
+//     }
+//   }
+// };
 
-document.addEventListener('DOMContentLoaded', () => {
-  const { DateTime } = luxon;
-  const parent = document.getElementById('date');
-  parent.innerText = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
-});
+
+
+
+
+                 // Luxon js
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const { DateTime } = luxon;
+//   const parent = document.getElementById('datetime');
+//   parent.innerText = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+// });
+
+
+
+
+          // I tried this for loop
+
+// const numberOfSection = document.getElementsByTagName("section").length;
+
+// for ( let i = 0; i<numberOfSection; i+1) {
+//   document.getElementsByTagName[i].addEventListener("click", () => {
+//     if ( numberOfSection.style.display === "none") {
+//       numberOfSection.style.display = "flex";
+//     } else {
+//       numberOfSection.style.display = "none"
+//     }
+//   });
+// }
+
+
+
+
+
+//                    I tried this below
+
+// const list = document.getElementsById("books-list");
+// const addlink = document.getElementsById("addBook-section");
+// const contactme = document.getElementsById("contact-section");
+
+// document.getElementById('list').addEventListener('click', () => {
+//   list.style.display = 'block';
+//   addlink.style.display = 'none';
+//   contactme.style.display = 'none';
+// });
+
+// document.getElementById('addlink').addEventListener('click', () => {
+//   list.style.display = 'none';
+//   addlink.style.display = 'block';
+//   contactme.style.display = 'none';
+// });
+
+// document.getElementById('contactme').addEventListener('click', () => {
+//   list.style.display = 'none';
+//   addlink.style.display = 'none';
+//   contactme.style.display = 'block';
+// });
+
+
+
+
+
+//      I  was    Checking if it could work
+
+// function hideOrShow() {
+//   var x = document.getElementById("books-list");
+
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none"
+//   }
+// }
